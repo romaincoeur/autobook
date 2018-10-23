f0b4c5cb1e828e3dd97212b66ef95ce9e1b60080
Romain Coeur <coeurro@gmail.com>
2018-10-23 10:55:34 +0200

    ...

--- scrpit.js
+++ scrpit.js
@@ -1,8 +1,15 @@
+// Usage
+// $ casperjs script.js --email=<your email> --password=<password>
+
+
 var casper = require('casper').create({
 	verbose: false,
 	logLevel: "debug",
 	pageSettings: { webSecurityEnabled: false }
 });
+
+if (!casper.cli.get('email') || !casper.cli.get('password'))
+	throw new Error("params email and password must be defined following this pattern \n casperjs script.js --email=<your email> --password=<password>");
 
 casper.start('https://espacemembre.movidaclub.fr/AdelyaClientSpe/movida/home.jsp');
 
@@ -14,7 +21,7 @@ casper.on('remote.message', function(message) {
 	    this.echo(message);
 });
 
-casper.waitForSelector("input[type='email']", function() {
+casper.waitForSelector("input[type='email']", function login() {
 	this.fillSelectors('div.connectZone', {
 		'input[type = email ]' : casper.cli.get('email'),
 		'input[type = password ]' : casper.cli.get('password')
@@ -24,18 +31,16 @@ casper.waitForSelector("input[type='email']", function() {
 });
 
 
-casper.waitFor(function(){
-	return !this.exists('div#makeresa.disabled');
+casper.waitFor(function goToCalendar(){
+	return this.exists('div.sprite-FITNESS');
 }, function() {
 	console.log("Moving to makeresa screen...");
 	this.click('div#makeresa');
 	console.log('Done');
 });
 
-casper.then(function(){
-	this.reload(function(){
+casper.then(function clickTimeslot(){
 		console.log(this.getHTML('section#section_makeresa #full_categories'));
-	});
 });
 
 casper.waitForSelector("div.sprite-FITNESS", function() {
